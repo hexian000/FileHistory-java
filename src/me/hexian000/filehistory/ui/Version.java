3c32b31badc7ab11ab901fe78c37bc6566d5af97
hexian000 <he.xian@163.com>
2019-02-01 15:25:35 +0800

    version bump to 0.9-beta

--- src/me/hexian000/filehistory/ui/Version.java
+++ src/me/hexian000/filehistory/ui/Version.java
@@ -2,11 +2,11 @@ package me.hexian000.filehistory.ui;
 
 final class Version {
 	static final String TITLE = "File History";
-	static final String COPYRIGHT = "By: He Xian Copyright (c) 2018";
+	static final String COPYRIGHT = "By: He Xian Copyright (c) 2018-2019";
 	private static final int MAJOR = 0;
-	private static final int MINOR = 4;
+	private static final int MINOR = 9;
 	private static final int REVISION = 0;
-	private static final String TAG = "alpha";
+	private static final String TAG = "beta";
 
 	static String format() {
 		StringBuilder sb = new StringBuilder();
@@ -14,6 +14,7 @@ final class Version {
 		if (REVISION != 0) {
 			sb.append('.').append(REVISION);
 		}
+		//noinspection ConstantConditions
 		if (TAG.length() > 0) {
 			sb.append('-').append(TAG);
 		}
